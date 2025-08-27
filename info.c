763fa3181d3feb7815c56658c18712a8bf56eaa4
x011-al <x011pagkor@gmail.com>
2025-08-27 04:45:53 -0700

    Create info.c

---
+++ info.c
@@ -0,0 +1,26 @@
+// file: info_license.c
+#include <stdio.h>
+
+int main(void) {
+    printf("=============================================================\n");
+    printf("                INFORMASI LISENSI & PENGATURAN MINER         \n");
+    printf("=============================================================\n\n");
+    printf("Kode miner ini memiliki LICENSE dan memerlukan setting wallet user.\n");
+    printf("Untuk mendapatkan lisensi lifetime penggunaan dan melakukan setting,\n");
+    printf("silahkan melakukan pembayaran license:\n\n");
+
+    printf("  License  : Rp.100.000\n");
+    printf("  No. Rek  : 108832314304\n");
+    printf("  Bank     : Jago\n");
+    printf("  WA       : 081553020811\n\n");
+
+    printf("Jika sudah melakukan pembayaran license, silahkan hubungi kontak WA\n");
+    printf("dengan melampirkan bukti transfer valid. Repo miner baru dengan lisensi\n");
+    printf("dan wallet minermu akan segera dikirimkan.\n\n");
+
+    printf("Terima kasih sudah mendukung pengembangan kami. Regards.\n");
+    printf("=============================================================\n");
+    printf("Tekan ENTER untuk keluar...");
+    getchar();
+    return 0;
+}
